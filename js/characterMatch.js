7e82c6653c256f9a66d7844190360553f18fc419
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-26 14:31:53 -0700

    solved in js

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,17 @@
 exports.isCharacterMatch = function(string1, string2) {
 
+    string1 = string1.toLowerCase().replaceAll(" ", "").split("");
+    string2 = string2.toLowerCase().replaceAll(" ", "").split("");
+
+    if (!(string1.length === string2.length)) {
+        return false;
+    }
+
+    for (let i=0; i<string1.length; i++) {
+        if (string2.includes(string1[i])) {
+            string2 = string2.join("").replace(string1[i],"");
+            string2 = string2.split("")
+        }
+        }
+    return (string2.length == 0) ? true : false;
 };
