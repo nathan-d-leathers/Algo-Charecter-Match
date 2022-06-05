505a32bbe8e3e77a1928ab89acae167a56508d47
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-04 19:52:34 -0700

    solved in js and py

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,22 @@
 # Don't forget to run your tests!
-
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	
+ string1 = string1.lower().replace(" ","")
+ string2 = string2.lower().replace(" ","")
+ new_str_1 = []
+ new_str_2 = []
+    
+ for x in string1:
+     new_str_1.append(x)
+ for y in string2:
+     new_str_2.append(y)
+        
+ new_str_1.sort()
+ new_str_2.sort()
+ string_one = "".join(new_str_1)
+ string_two = "".join(new_str_2)
+    
+ if string_one == string_two:
+     return True
+ else:
+     return False
